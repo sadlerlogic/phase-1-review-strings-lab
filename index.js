c9b22084f1a3cc199937b2276ef614deef69b9cb
Christopher Sadler <sadlerlogic@gmail.com>
2022-02-01 19:18:08 -0600

    commit tree 6dd9d95ae40af8db834a537882bde94d0f7b8903

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Grace Jimmy Jones';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
